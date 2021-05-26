f942b855c486db1f4d8c0adc5e255fe397c337f4
Jacob Walser <jwalser90@gmail.com>
2021-05-26 17:02:51 -0400

    test-icm20602: add proper signal handling

--- test-icm20602.py
+++ test-icm20602.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-from icm20602 import ICM20602
 import argparse
+from icm20602 import ICM20602
+import signal
 import time
 
 parser = argparse.ArgumentParser(description='ads1115 test')
@@ -11,22 +12,26 @@ args = parser.parse_args()
 
 icm = ICM20602()
 
+outfile = None
+
 if args.output:
     outfile = open(args.output, "w")
+
+def cleanup(_signo, _stack):
+    if outfile:
+        outfile.close()
+    exit(0)
+
+signal.signal(signal.SIGTERM, cleanup)
+signal.signal(signal.SIGINT, cleanup)
 
 while True:
     data = icm.read_all()
     output =   ( f'{time.time()} 1 {data.a.x} {data.a.y} {data.a.z} {data.g.x} {data.g.y} {data.g.z} {data.t} '
                  f'{data.a_raw.x} {data.a_raw.y} {data.a_raw.z} {data.g_raw.x} {data.g_raw.y} {data.g_raw.z} {data.t_raw}' )
     print(output)
-    if args.output:
+    if outfile:
         outfile.write(output)
         outfile.write('\n')
     if args.frequency:
         time.sleep(1.0/args.frequency)
-
-# this is never reached, but works anyway in practice
-# todo handle KeyboardInterrupt for ctrl+c
-if args.output:
-    outfile.close()
-
