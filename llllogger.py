769a2a5ba4a530960ed125d070951eba02ed1b10
Jacob Walser <jwalser90@gmail.com>
2021-07-19 02:40:05 -0400

    going great

--- llllogger.py
+++ llllogger.py
@@ -11,6 +11,7 @@ import typing
 from io import StringIO
 
 # class LLAxis:
+# https://pandas.pydata.org/pandas-docs/stable/development/extending.html
 @pd.api.extensions.register_series_accessor("ll")
 class LLAxis:
     def __init__(self, pandas_obj):
@@ -18,7 +19,6 @@ class LLAxis:
 
     def plot(self, ax=None):
         meta = self._obj.attrs['llMeta']
-
         print('got meta')
         print(meta)
         name = meta['name']
@@ -26,40 +26,21 @@ class LLAxis:
             units = f'({meta["units"]})'
         except KeyError:
             units = ''
-        
-
         try:
             color = f'{meta["color"]}'
         except KeyError:
             # todo random, or colormap
             color = 'black'
-
         try:
             marker = f'{meta["marker"]}'
         except KeyError:
             marker = 'x'
-
         if ax is None:
             ax = self._obj.plot(c=color, marker=marker)
         else:
             ax = self._obj.plot(c=color, marker=marker, secondary_y=True, mark_right=False, ax=ax)
         ax.set_ylabel(f'{name}{units}')
         return ax
-
-
-
-
-
-# frozen (read only)?
-@dataclass
-class LLogDesc:
-    data: typing.TextIO
-
-    def __post_init__(self):
-        print(self.data)
-        setattr(self, 'n', 2)
-        self.df = pd.read_csv(self.data, sep=',', header=None)
-
 
 class LLogReader:
     def __init__(self, logfile, metafile):
@@ -78,7 +59,6 @@ class LLogReader:
             value = DF[DF['llType'] == int(llType)].dropna(axis='columns', how='all')
 
             try:
-
                 columns = llDesc['columns']
                 for c in range(len(columns)):
                     i = c + 2
@@ -89,13 +69,10 @@ class LLogReader:
                 for c in range(len(columns)):
                     name = columns[c]['name']
                     value[name] = value[name].astype(float)
-                
                 # attach metadata !! this must be done last
                 for c in range(len(columns)):
                     name = columns[c]['name']
                     value[name].attrs['llMeta'] = columns[c]
-                
-            
             except ValueError:
                 # 'could not convert stringt o flouat'
                 print(f'{attr} could not convert string to float')
@@ -103,15 +80,10 @@ class LLogReader:
             except KeyError:
                 print(f'{attr} does not have columns definition')
 
-
-
-
-            
             # eg for each type name in log, set self.type to
             # the dataframe representing only that type
             print(f'setting {attr} to \n{value}')
             setattr(self, attr, value)
-
 
     def metaOpen(self, metafile):
         print(metafile)
@@ -120,55 +92,19 @@ class LLogReader:
     
     def logOpen(self, logfile):
         return pd.read_csv(logfile,sep=',', header=None).drop(index=0).dropna(axis='columns', how='all')
-        
-    def metaByName(self, name):
-        pass
-
-    def dfByName(self, name):
-        pass
 
 
 ll = LLogReader('bme.csv', 'bme.meta')
-# df = ll.dfByName('data')
 
 data = ll.data
 pressure = data.pressure
-
-meta = pressure.attrs['llMeta']
-name = meta['name']
-units = meta['units']
-color = meta['color']
-marker = meta['marker']
-
-
-# data.plot()
-ax = pressure.plot(c=color)
-ax.set_ylabel(f'{name} ({units})')
-
-# https://pandas.pydata.org/pandas-docs/stable/development/extending.html
 temperature = data.temperature
-
-meta = temperature.attrs['llMeta']
-name = meta['name']
-units = meta['units']
-color = meta['color']
-marker = meta['marker']
-ax2 = temperature.plot(c=color, secondary_y=True, mark_right=False)
-ax2.set_ylabel(f'{name} ({units})')
-
-plt.show()
-
-print(temperature)
-
-
-# ax = df[['gx', 't']].plot()
-# ax.set_ylabel(df['gx'].units)
-# df[['gx', 'gy', 'gz']].plot(secondary_y=True, ax=ax)
-
-# plt.show()
 
 ax = pressure.ll.plot()
 data.pressure_raw.ll.plot()
 temperature.ll.plot(ax)
+
+plt.figure()
+temperature.ll.plot()
 
 plt.show()
