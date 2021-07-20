53f4598cf1356f7db618ddd49736f3f851cc6c85
Jacob Walser <jwalser90@gmail.com>
2021-07-19 22:08:13 -0400

    wip, end

--- test-icm20602.py
+++ test-icm20602.py
@@ -2,7 +2,7 @@
 
 import argparse
 from icm20602 import ICM20602
-from llogger import LLogger
+from llog import *
 import signal
 import time
 
@@ -12,62 +12,7 @@ parser.add_argument('--frequency', action='store', type=int, default=None)
 args = parser.parse_args()
 
 
-LLOG_ERROR = 0
-# read only memory + factory calibration and serialization type information
-LLOG_ROM = 1
-# application-specific configuration information
-LLOG_CONFIG = 2
-# calibration data
-LLOG_CALIBRATION = 3
-# measurement data
-LLOG_DATA = 4
-
-
-def icmFormat(data):
-    return (f'{data.a.x} {data.a.y} {data.a.z} {data.g.x} {data.g.y} {data.g.z} {data.t} '
-            f'{data.a_raw.x} {data.a_raw.y} {data.a_raw.z} {data.g_raw.x} {data.g_raw.y} {data.g_raw.z} {data.t_raw}')
-
-categories = {
-    LLOG_ERROR: {
-        'name': 'error',
-        'columns': [
-            ['error code', '-']
-        ]
-    },
-    LLOG_ROM: {
-        'name': 'rom',
-        'columns': [
-            ['month', '-'],
-            ['day', '-'],
-            ['year', '-'],
-            ['pmin', 'bar'],
-            ['pmax', 'bar'],
-            ['pmode', '[PA,PR,PAA]']
-        ]
-    },
-    LLOG_DATA: {
-        'name': 'measurement',
-        'columns': [
-            ['ax', 'g'],
-            ['ay', 'g'],
-            ['az', 'g'],
-            ['gx', 'dps'],
-            ['gy', 'dps'],
-            ['gz', 'dps'],
-            ['temperature', 'C'],
-            ['ax_raw', 'g'],
-            ['ay_raw', 'g'],
-            ['az_raw', 'g'],
-            ['gx_raw', 'dps'],
-            ['gy_raw', 'dps'],
-            ['gz_raw', 'dps'],
-            ['temperature_raw', 'C'],
-        ],
-        'format': icmFormat
-    },
-}
-
-log = LLogger(categories, console=True, logfile=args.output)
+log = LLogWriter(categories, console=True, logfile=args.output)
 
 def cleanup(_signo, _stack):
     log.close()
@@ -76,13 +21,17 @@ def cleanup(_signo, _stack):
 signal.signal(signal.SIGTERM, cleanup)
 signal.signal(signal.SIGINT, cleanup)
 
+# format data object to csv string for llog
+def format(data):
+    return (f'{data.a.x} {data.a.y} {data.a.z} {data.g.x} {data.g.y} {data.g.z} {data.t} '
+            f'{data.a_raw.x} {data.a_raw.y} {data.a_raw.z} {data.g_raw.x} {data.g_raw.y} {data.g_raw.z} {data.t_raw}')
 
 icm = ICM20602()
 
 while True:
     try:
         data = icm.read_all()
-        log.log(LLOG_DATA, data)
+        log.log(LLOG_DATA, format(data))
     except Exception as e:
         log.log(LLOG_ERROR, e)
 
