cbbf8b3021f856b10d6114722fb7a40478fb764d
Jacob Walser <jwalser90@gmail.com>
2021-07-22 03:43:13 -0400

    so close, wth meta arg

--- test.py
+++ test.py
@@ -15,58 +15,115 @@ class LLogSeries(pd.Series):
     def _constructor_expanddim(self):
         return LLogDataFrame
 
-    def pplot(self, *args, **kwargs):
+    def pplot(self, ll2=None, *args, **kwargs):
         print(f'sup {self.name}')
 
         # print(self.index.get_loc(self.name))
         meta = self.meta[self.name]
-        kwargs = kwargs | {'label': self.name}
+        
+        kwargs2 = kwargs | {'label': self.name}
 
         for opt in ["color", "style", "label"]:
             try:
-                kwargs = kwargs | {opt:meta[opt]}
+                kwargs2 = kwargs2 | {opt:meta[opt]}
             except KeyError as e:
                 # print(e)
                 pass
 
-        self.plot(*args, **kwargs)
+        self.plot(*args, **kwargs2)
         plt.legend()
         plt.ylabel(meta['units'])
+
+        if ll2 is not None:
+            plt.twinx()
+            ll2.pplot(*args, **kwargs)
         
     
 # https://stackoverflow.com/questions/48325859/subclass-pandas-dataframe-with-required-argument
 class LLogDataFrame(pd.DataFrame):
     _metadata = ['meta']
 
+
+    def __init__(self, *args, **kwargs):
+        # grab the keyword argument that is supposed to be my_attr
+        self.meta = kwargs.pop('meta', None)
+        columns = self.meta['columns']
+        print("hello", columns)
+
+        # for c in df[1:]:
+        # #     print(c)
+        # l = min(len(columns), len(self)-2)
+
+        # for c in range(l):
+        #     llabel = columns[i]['llabel']
+        #     print(llabel, self[c], self[c+2])
+        #     self[c+2].rename(llabel, inplace=True)
+
+            # value.rename(columns={i+2: name}, inplace=True)
+            # value[name].llSeriesMeta = columns[i]
+            # print(f'!!!! {i} {value[name].llSeriesMeta}')
+        super().__init__(*args, **kwargs)
+
+    # @propertydf
+    # def _constructor(self):
+    #     return LLogDataFrame
+
+
     @property
     def _constructor(self):
-        return LLogDataFrame
+        """This is the key to letting Pandas know how to keep
+        derivative `SomeData` the same type as yours.  It should
+        be enough to return the name of the Class.  However, in
+        some cases, `__finalize__` is not called and `my_attr` is
+        not carried over.  We can fix that by constructing a callable
+        that makes sure to call `__finlaize__` every time."""
+        def _c(*args, **kwargs):
 
+            # a = LLogDataFrame(*args, **kwargs).__finalize__(self)
+            a = LLogDataFrame(*args, meta=self.meta, **kwargs)
+            print('fffuck')
+            print(len(a.columns))
+            # a.rename(...)
+            return a
+
+        return _c
+        
     @property
     def _constructor_sliced(self):
         print('fuck')
         return LLogSeries
 
-    def pplot(self, *args, **kwargs):
+    def pplot(self, d2=None, *args, **kwargs):
         for c in self:
             self[c].pplot(*args, **kwargs)
+        # plot things on secondary axis
+        if d2 is not None:
+            plt.twinx()
+            d2.pplot(*args, **kwargs)
 
 # df = LLogDataFrame({1: [1,2], 1:[2,4], 3:[3,6]}, metadata, index=None)
-df = LLogDataFrame({1: [1,2], 1:[2,4], 3:[3,6]}, index=None)
-df = LLogDataFrame({1: [1,2], 2:[2,4], 3:[3,6]}, index=None)
-
 dfmeta = {
     "llType": "data",
     "columns": [
-        {"label": "gx", "style": "x-", "units": "C"},
-        {"label": "gy", "units": "dps"},
-        {"label": "gz", "style":"o", "color": "green", "units": "dps"}
+        {"llabel": "gx", "style": "x-", "units": "C"},
+        {"llabel": "gy", "units": "dps"},
+        {"llabel": "gz", "style":"o", "color": "green", "units": "dps"}
     ]
 }
+df = LLogDataFrame({1: [1,2], 2:[2,4], 3:[3,6]}, meta=dfmeta, index=None)
 
-df.meta = dfmeta
+# dfmeta = {
+#     "llType": "data",
+#     "columns": [
+#         {"label": "gx", "style": "x-", "units": "C"},
+#         {"label": "gy", "units": "dps"},
+#         {"label": "gz", "style":"o", "color": "green", "units": "dps"}
+#     ]
+# }
 
-df2 = df[['gx', 'gy']]
+# df.meta = dfmeta
 
-df.pplot()
-plt.show()
+# df2 = df[['gx', 'gy']]
+
+# df.pplot()
+# plt.show()
