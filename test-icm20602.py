c79dc7b5e3e434283840ceef3aa7d90074d38cce
ES-Alexander <sandman.esalexander@gmail.com>
2021-09-13 15:44:24 +1000

    test-icm20602: update to use `LLogWriter`

--- test-icm20602.py
+++ test-icm20602.py
@@ -2,36 +2,25 @@
 
 import argparse
 from icm20602 import ICM20602
-import signal
+from pathlib import Path
+import llog
 import time
 
-parser = argparse.ArgumentParser(description='ads1115 test')
+device = "icm20602"
+defaultMeta = Path(__file__).resolve().parent / f"{device}.meta"
+
+parser = argparse.ArgumentParser(description=f'{device} test')
 parser.add_argument('--output', action='store', type=str, default=None)
+parser.add_argument('--meta', action='store', type=str, default=defaultMeta)
 parser.add_argument('--frequency', action='store', type=int, default=None)
 args = parser.parse_args()
 
-icm = ICM20602()
+with llog.LLogWriter(args.meta, args.output) as log:
+    icm = ICM20602()
 
-outfile = None
-
-if args.output:
-    outfile = open(args.output, "w")
-
-def cleanup(_signo, _stack):
-    if outfile:
-        outfile.close()
-    exit(0)
-
-signal.signal(signal.SIGTERM, cleanup)
-signal.signal(signal.SIGINT, cleanup)
-
-while True:
-    data = icm.read_all()
-    output =   ( f'{time.time()} 1 {data.a.x} {data.a.y} {data.a.z} {data.g.x} {data.g.y} {data.g.z} {data.t} '
-                 f'{data.a_raw.x} {data.a_raw.y} {data.a_raw.z} {data.g_raw.x} {data.g_raw.y} {data.g_raw.z} {data.t_raw}' )
-    print(output)
-    if outfile:
-        outfile.write(output)
-        outfile.write('\n')
-    if args.frequency:
-        time.sleep(1.0/args.frequency)
+    while True:
+        data = icm.read_all()
+        log.log(llog.LLOG_DATA, (f'{data.a.x} {data.a.y} {data.a.z} {data.g.x} {data.g.y} {data.g.z} {data.t} '
+                f'{data.a_raw.x} {data.a_raw.y} {data.a_raw.z} {data.g_raw.x} {data.g_raw.y} {data.g_raw.z} {data.t_raw}'))
+        if args.frequency:
+            time.sleep(1.0/args.frequency)
