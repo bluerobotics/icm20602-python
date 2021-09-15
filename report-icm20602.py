99ab3874edcafbd5c41f8f834a8601ce6a6e0215
Jacob Walser <jwalser90@gmail.com>
2021-09-14 22:18:54 -0400

    report-icm20602: update/refactor

--- report-icm20602.py
+++ report-icm20602.py
@@ -22,29 +22,14 @@ an = log.data[['ax', 'ay', 'az']]
 gn = log.data[['gx', 'gy', 'gz']]
 t = log.data.t
 
-pdf = None
-if args.output:
-    outfile = open('icm20602.pdf', 'wb')
-    pdf = PdfPages(outfile)
-
-def saveFig():
-    if pdf:
-        pdf.savefig()
-
-def closeFig():
-    if pdf:
-        pdf.close()
-
 footer = 'icm20602 test report'
 
 f, spec = log.figure(height_ratios=[1,1], suptitle='icm20602 data statistics', footer=footer)
 plt.subplot(spec[0,:])
-an.stats().table(rl=True)
+an.stats().ttable(rl=True)
 
 plt.subplot(spec[1,:])
-gn.stats().table(rl=True)
-
-saveFig()
+gn.stats().ttable(rl=True)
 
 f, spec = log.figure(height_ratios=[1,1,1], suptitle='icm20602 data plots', footer=footer)
 
@@ -57,9 +42,10 @@ gn.pplot()
 plt.subplot(spec[2,:])
 t.pplot()
 
-saveFig()
-
-closeFig()
+if args.output:
+    # todo check if it exists!
+    with PdfPages(args.output) as pdf:
+        [pdf.savefig(n) for n in plt.get_fignums()]
 
 if args.show:
     plt.show()
