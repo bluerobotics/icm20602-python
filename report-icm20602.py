04a27f4fd2a5003b197c0a8e6f647a68437e5583
ES-Alexander <sandman.esalexander@gmail.com>
2021-09-23 12:56:57 +1000

    report-icm20602: use default parser

--- report-icm20602.py
+++ report-icm20602.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
 
-import argparse
 from llog import LLogReader
 from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
-import os
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
-defaultMeta = dir_path + '/icm20602.meta'
-parser = argparse.ArgumentParser(description='icm20602 test report')
-parser.add_argument('--input', action='store', type=str, required=True)
-parser.add_argument('--meta', action='store', type=str, default=defaultMeta)
-parser.add_argument('--output', action='store', type=str)
-parser.add_argument('--show', action='store_true')
+DEVICE = 'icm20602'
+parser = LLogReader.create_default_parser(__file__, DEVICE)
 args = parser.parse_args()
 
 log = LLogReader(args.input, args.meta)
@@ -22,16 +14,16 @@ an = log.data[['ax', 'ay', 'az']]
 gn = log.data[['gx', 'gy', 'gz']]
 t = log.data.t
 
-footer = 'icm20602 test report'
+footer = f'{DEVICE} test report'
 
-f, spec = log.figure(height_ratios=[1,1], suptitle='icm20602 data statistics', footer=footer)
+f, spec = log.figure(height_ratios=[1,1], suptitle=f'{DEVICE} data statistics', footer=footer)
 plt.subplot(spec[0,:])
 an.stats().ttable(rl=True)
 
 plt.subplot(spec[1,:])
 gn.stats().ttable(rl=True)
 
-f, spec = log.figure(height_ratios=[1,1,1], suptitle='icm20602 data plots', footer=footer)
+f, spec = log.figure(height_ratios=[1,1,1], suptitle=f'{DEVICE} data plots', footer=footer)
 
 plt.subplot(spec[0,:])
 an.pplot()
