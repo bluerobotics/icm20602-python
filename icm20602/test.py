37216cbabcdaac7281d90ecd6b60b17925f0e569
ES-Alexander <sandman.esalexander@gmail.com>
2021-09-23 12:54:04 +1000

    test-icm20602: simplify with abstracted logging

--- icm20602/test.py
+++ icm20602/test.py
@@ -1,30 +1,20 @@
 #!/usr/bin/python3
 
-def main():
-    import argparse
-    from icm20602 import ICM20602
-    from pathlib import Path
-    import llog
-    import time
+from icm20602 import ICM20602
+from llog import LLogWriter
+import time
 
-    device = "icm20602"
-    defaultMeta = Path(__file__).resolve().parent / f"{device}.meta"
+device = "icm20602"
+parser = LLogWriter.create_default_parser(__file__, device)
+args = parser.parse_args()
 
-    parser = argparse.ArgumentParser(description=f'{device} test')
-    parser.add_argument('--output', action='store', type=str, default=None)
-    parser.add_argument('--meta', action='store', type=str, default=defaultMeta)
-    parser.add_argument('--frequency', action='store', type=int, default=None)
-    args = parser.parse_args()
+with llog.LLogWriter(args.meta, args.output) as log:
+    icm = ICM20602()
+    
+    def data_getter():
+        data = icm.read_all()
+        return (f'{data.a.x} {data.a.y} {data.a.z} {data.g.x} {data.g.y} {data.g.z} {data.t} '
+                f'{data.a_raw.x} {data.a_raw.y} {data.a_raw.z} '
+                f'{data.g_raw.x} {data.g_raw.y} {data.g_raw.z} {data.t_raw}')
 
-    with llog.LLogWriter(args.meta, args.output) as log:
-        icm = ICM20602()
-
-        while True:
-            data = icm.read_all()
-            log.log(llog.LLOG_DATA, (f'{data.a.x} {data.a.y} {data.a.z} {data.g.x} {data.g.y} {data.g.z} {data.t} '
-                    f'{data.a_raw.x} {data.a_raw.y} {data.a_raw.z} {data.g_raw.x} {data.g_raw.y} {data.g_raw.z} {data.t_raw}'))
-            if args.frequency:
-                time.sleep(1.0/args.frequency)
-
-if __name__ == '__main__':
-    main()
+    log.log_data_loop(data_getter, parser_args=args)
