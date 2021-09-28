f2c6ccfc7634db46925550c71f02c6f1693bd4b7
Jacob Walser <jwalser90@gmail.com>
2021-09-28 07:18:25 -0400

    test: wrap main for setup console_scripts

--- icm20602/test.py
+++ icm20602/test.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 
-from icm20602 import ICM20602
-from llog import LLogWriter
-import time
+def main():
+    from icm20602 import ICM20602
+    from llog import LLogWriter
+    import time
 
-device = "icm20602"
-parser = LLogWriter.create_default_parser(__file__, device)
-args = parser.parse_args()
+    device = "icm20602"
+    parser = LLogWriter.create_default_parser(__file__, device)
+    args = parser.parse_args()
 
-with LLogWriter(args.meta, args.output) as log:
-    icm = ICM20602()
-    
-    def data_getter():
-        data = icm.read_all()
-        return (f'{data.a.x} {data.a.y} {data.a.z} {data.g.x} {data.g.y} {data.g.z} {data.t} '
-                f'{data.a_raw.x} {data.a_raw.y} {data.a_raw.z} '
-                f'{data.g_raw.x} {data.g_raw.y} {data.g_raw.z} {data.t_raw}')
+    with LLogWriter(args.meta, args.output) as log:
+        icm = ICM20602()
 
-    log.log_data_loop(data_getter, parser_args=args)
+        def data_getter():
+            data = icm.read_all()
+            return (f'{data.a.x} {data.a.y} {data.a.z} {data.g.x} {data.g.y} {data.g.z} {data.t} '
+                    f'{data.a_raw.x} {data.a_raw.y} {data.a_raw.z} '
+                    f'{data.g_raw.x} {data.g_raw.y} {data.g_raw.z} {data.t_raw}')
+
+        log.log_data_loop(data_getter, parser_args=args)
+
+if __name__ == '__main__':
+    main()
